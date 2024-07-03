99ae122879bc31733c5cdb931bf5e4e5cd8f4d69
Megan Fung <emailmeganfung@gmail.com>
2024-07-02 21:51:13 -0700

    updated action schema and GPT instructions

--- testing.py
+++ testing.py
@@ -1,14 +1,12 @@
 import requests
 
-url = 'https://ppp-ahowny142-megan-fungs-projects.vercel.app/run_python'
+url = ' https://ppp-75tz6pbx2-megan-fungs-projects.vercel.app/run_python'
 files = {'file': open('Medium Excel Sample.xlsx', 'rb')}
 data = {'pg': None}
 headers = {'x-vercel-protection-bypass': 'UmZ9Iux92fppn1pOAwuMTKFglDmA0PC2'}
 
 response = requests.post(url, files=files, data=data, headers=headers)
 print('Response status code:', response.status_code)
-print('Response text:', response.text)
-
 try:
     json_response = response.json()
     print("JSON Response:", json_response)
