a4b2a41e766465ab232aeadaaf04d0ae220fe056
Megan Fung <emailmeganfung@gmail.com>
2024-07-03 05:00:42 -0700

    updated action schema and GPT instructions

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import requests
 
-url = ' https://ppp-75tz6pbx2-megan-fungs-projects.vercel.app/run_python'
+url = 'https://ppp-hi03zaoki-megan-fungs-projects.vercel.app/run_python'
 files = {'file': open('Medium Excel Sample.xlsx', 'rb')}
 data = {'pg': None}
 headers = {'x-vercel-protection-bypass': 'UmZ9Iux92fppn1pOAwuMTKFglDmA0PC2'}
