4ff0ca60b9a84bb9b868761397f4a0288c0eb862
Megan Fung <emailmeganfung@gmail.com>
2024-07-08 09:59:15 -0700

    updated action schema and instructions. Authentication issue fixed.

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 import requests
 
-url = 'https://ppp-hi03zaoki-megan-fungs-projects.vercel.app/run_python'
+url = 'https://ppp-i1rl8289o-megan-fungs-projects.vercel.app/run_python'
 files = {'file': open('Medium Excel Sample.xlsx', 'rb')}
 data = {'pg': None}
-headers = {'x-vercel-protection-bypass': '<get from vercel>'}
+headers = {'x-vercel-protection-bypass': 'UmZ9Iux92fppn1pOAwuMTKFglDmA0PC2'}
 
 response = requests.post(url, files=files, data=data, headers=headers)
 print('Response status code:', response.status_code)
